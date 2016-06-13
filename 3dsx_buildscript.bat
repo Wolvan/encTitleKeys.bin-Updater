f56b61a163157e92adda9739023232ca88896fe8
Wolvan <wolvan1@gmail.com>
2016-06-13 05:30:58 +0200

    Remove all build tools from project root
    These do not belong in the repository root either and just make the
    directory structure a total mess

--- 3dsx_buildscript.bat
+++
@@ -1,2 +0,0 @@
-3dstool -cvtf romfs romfs.bin --romfs-dir romfs
-3dsxtool lpp-3ds.elf lpp-3ds.3dsx --romfs=romfs.bin --smdh=lpp-3ds.smdh
--- cia_buildscript.bat
+++
@@ -1,2 +0,0 @@
-3dstool -cvtf romfs romfs.bin --romfs-dir romfs
-makerom -f cia -o lpp-3ds.cia -elf lpp-3ds.elf -rsf cia_workaround.rsf -icon icon.bin -banner banner.bin -exefslogo -target t -romfs romfs.bin
