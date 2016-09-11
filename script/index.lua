ca3dbd31af9140f57e63a42864abd83c9bf4c40a
Wolvan <wolvan1@gmail.com>
2016-09-11 17:48:19 +0200

    Bump Version to 1.6.0

--- script/index.lua
+++ script/index.lua
@@ -7,7 +7,7 @@ local YELLOW = Color.new(255,205,66)
 local RED = Color.new(255,0,0)
 local GREEN = Color.new(55,255,0)
 
-local APP_VERSION = "1.5.0"
+local APP_VERSION = "1.6.0"
 local APP_DIR = "/3ds/data/titlekeysTools"
 local APP_CONFIG = APP_DIR.."/config.json"
 local APP_LIBS_DIR = APP_DIR.."/Libraries"
